ca1fd4ae2c863da272c9ef25b6c6bfe1e292affb
Keith Rocheck <749812+krocheck@users.noreply.github.com>
2020-11-07 21:19:34 -0500

    refactor to es6 - fix feedback

--- index.js
+++ index.js
@@ -1,344 +1,332 @@
 var instance_skel = require('../../instance_skel');
-var debug;
-var log;
-var imagelist = [];
-var request = require('request').defaults({ encoding: null });
-var uploadimg = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg==";
-var ipadr;
-function instance(system, id, config) {
-	var self = this;
 
-	// super-constructor
-	instance_skel.apply(this, arguments);
+/**
+ * Companion instance class for Ross Video Xpression.
+ *
+ * @extends instance_skel
+ * @since 1.0.0
+ * @author Nijs Jonas
+ * @author yyy898
+ * @author Keith Rocheck <keith.rocheck@gmail.com>
+ */
+class instance extends instance_skel {
 
-	self.actions(); // export actions
-	self.init_feedbacks();
-	return self;
-}
-instance.prototype.updateConfig = function(config) {
-	var self = this;
-	self.config = config;
-	self.actions();
-	if (self.config.IP !== undefined) {
-		var cmd = "http://" + self.config.IP + ":4430/titan/get/System/SoftwareVersion";
-		self.system.emit('rest_get', cmd, function (err, result) {
-			if (err !== null) {
-				self.status(self.STATUS_ERROR, result.error.code);
-			}
-			else {
-				self.status(self.STATUS_OK);
-			}
-			ipadr = self.config.IP;
-	});	}
-}
-// Choices
-instance.prototype.CHOICES_CUELIST = [
-	{ label: 'GO', id: '0' },
-	{ label: 'GO BACK', id: '1' }
-];
-instance.prototype.CHOICES_ONOFF = [
-	{ label: 'ON', id: '0' },
-	{ label: 'OFF', id: '1' }
-];
-// Return config fields for web config
-instance.prototype.config_fields = function () {
-	var self = this;
-	return [
-		{
-			type: 'text',
-			id: 'info',
-			width: 12,
-			label: 'Information',
-			value: 'This module comunicates to avolites titan desks thru the webapi protocol, the desk needs to be running version 10.0 or higher for this to work, some features might only work with higher software versions so make sure your avolites desk is always up to date'
-		},
-		{
-			type: 'textinput',
-			id: 'IP',
-			label: 'Desk IP',
-			regex: self.REGEX_IP,
-			width: 12
-		}
-	]
-}
+	/**
+	 * Create an instance of an Xpression module.
+	 *
+	 * @param {EventEmitter} system - the brains of the operation
+	 * @param {string} id - the instance ID
+	 * @param {Object} config - saved user configuration parameters
+	 * @since 1.0.0
+	 */
+	constructor(system,id,config) {
+		super(system,id,config);
 
-// Startup test
-instance.prototype.init = function() {
-	var self = this;
-	ipadr = self.config.IP;
-	debug = self.debug;
-	log = self.log;
-	self.status(self.STATUS_ERROR);
-	if (self.config.IP !== undefined) {
-		var cmd = "http://" + ipadr + ":4430/titan/get/System/SoftwareVersion";
-		self.system.emit('rest_get', cmd, function (err, result) {
-			if (err !== null) {
-				self.status(self.STATUS_ERROR, result.error.code);
-			}
-			else {
-				self.status(self.STATUS_OK);
+		this.imageList = [];
+		this.pollInterval;
+		this.request = require('request').defaults({ encoding: null });
+
+		this.FIELD_CLACTION = {
+			type: 'dropdown',
+			label: 'Action',
+			id: 'cuelistaction',
+			default: '0',
+			choices: [
+				{ label: 'GO', id: '0' },
+				{ label: 'GO BACK', id: '1' }
+			]
+		};
+
+		this.FIELD_CUENUMBER = {
+			type: 'number',
+			label: 'CueNumber',
+			id: 'cn',
+			default: 1,
+			min: 1,
+			max: 9999
+		};
+
+		this.FIELD_PERCENTAGE = {
+			type: 'number',
+			label: 'Percentage (0->100)',
+			id: 'percentage',
+			default: 100,
+			min: 0,
+			max: 100
+		};
+
+		this.FIELD_PLAYBACKACTION = {
+			type: 'dropdown',
+			label: 'ON/OFF',
+			id: 'playbackaction',
+			default: '0',
+			choices: [
+				{ label: 'ON', id: '0' },
+				{ label: 'OFF', id: '1' }
+			]
+		};
+
+		this.FIELD_USERNUMBER = {
+			type: 'number',
+			label: 'UserNumber',
+			id: 'un',
+			default: 1,
+			min: 1,
+			max: 9999
+		};
+
+		this.actions(); // export actions
+		this.initFeedbacks();
+	}
+
+	/**
+	 * Setup the actions.
+	 *
+	 * @param {EventEmitter} system - the brains of the operation
+	 * @access public
+	 * @since 1.0.0
+	 */
+	actions(system) {
+
+		this.setActions({
+			'playbackAtPercentage': {
+				label: 'Playback @ Percentage',
+				options: [
+					this.FIELD_USERNUMBER,
+					this.FIELD_PERCENTAGE
+				],
+				callback: (action) => {
+					var percentage = action.options.percentage;
+					percentage = percentage/100;
+					this.sendCommand("script/2/Playbacks/FirePlaybackAtLevel?handle_userNumber=" + action.options.un + "&level_level=" + percentage + "&alwaysRefire=true");
+				}
+			},
+			'playbackFlash': {
+				label: 'Playback Flash',
+				options: [
+					this.FIELD_USERNUMBER,
+					this.FIELD_PLAYBACKACTION
+				],
+				callback: (action) => {
+					var percentage = "1";
+					if (action.options.playbackaction == '1')
+					{
+						percentage = "0";
+					}
+					this.sendCommand("script/2/Playbacks/FirePlaybackAtLevel?handle_userNumber=" + action.options.un + "&level_level=" + percentage + "&alwaysRefire=true");
+				}
+			},
+			'playbackSwop': {
+				label: 'Playback Swop',
+				options: [
+					this.FIELD_USERNUMBER,
+					this.FIELD_PLAYBACKACTION
+				],
+				callback: (action) => {
+					var playbackaction = "SwopPlayback";
+					if (action.options.playbackaction == '1')
+					{
+						playbackaction = "ClearSwopPlayback";
+					}
+					this.sendCommand("script/2/Playbacks/" + playbackaction + "?handle_userNumber=" + action.options.un);
+				}
+			},
+			'cuelistGo': {
+				label: 'Cuelist GO / BACK',
+				options: [
+					this.FIELD_USERNUMBER,
+					this.FIELD_CLACTION
+				],
+				callback: (action) => {
+					var cuelistaction = "Play";
+					if (action.options.cuelistaction == '1')
+					{
+						cuelistaction = "GoBack";
+					}
+					this.sendCommand("script/2/CueLists/" + cuelistaction + "?handle_userNumber=" + action.options.un);
+				}
+			},
+			'cuelistGoto': {
+				label: 'Cuelist Go to cue',
+				options: [
+					this.FIELD_USERNUMBER,
+					this.FIELD_CUENUMBER
+				],
+				callback: (action) => {
+					var success = this.sendCommand("script/2/CueLists/SetNextCue?handle_userNumber=" + action.options.un + "&stepNumber=" + action.options.cn);
+
+					if (success === true) {
+						this.sendCommand("script/2/CueLists/Play?handle_userNumber=" + action.options.un);
+					}
+				}
 			}
 		});
 	}
-	self.checkFeedbacks()
-}
 
-// When module gets deleted
-instance.prototype.destroy = function() {
-	var self = this;
-	debug("destroy");
-}
-
-instance.prototype.actions = function(system) {
-	var self = this;
-	if ( self.config.IP !== undefined ) {
-		if ( self.config.IP.length > 0 ) {
-			urlLabel = 'URI';
-		}
-	}
-
-	self.setActions({
-		'playbackAtPercentage': {
-			label: 'Playback @ Percentage',
-			options: [
-				{
-					type: 'textinput',
-					label: 'UserNumber',
-					id: 'un',
-					default: '1',
-					regex:   self.REGEX_NUMBER
-				},
-				{
-					type: 'textinput',
-					label: 'Percentage (0->100)',
-					id: 'percentage',
-					default: '1',
-					regex:   self.REGEX_NUMBER
-				}
-			]
-		},
-		'playbackFlash': {
-			label: 'Playback Flash',
-			options: [
-				{
-					type: 'textinput',
-					label: 'UserNumber',
-					id: 'un',
-					default: '1',
-					regex:   self.REGEX_NUMBER
-				},
-				{
-					type: 'dropdown',
-					label: 'ON/OFF',
-					id: 'playbackaction',
-					default: '0',
-					choices: self.CHOICES_ONOFF
-				}
-			]
-		},
-		'playbackSwop': {
-			label: 'Playback Swop',
-			options: [
-				{
-					type: 'textinput',
-					label: 'UserNumber',
-					id: 'un',
-					default: '1',
-					regex:   self.REGEX_NUMBER
-				},
-				{
-					type: 'dropdown',
-					label: 'ON/OFF',
-					id: 'playbackaction',
-					default: '0',
-					choices: self.CHOICES_ONOFF
-				}
-			]
-		},
-		'cuelistGo': {
-			label: 'Cuelist GO / BACK',
-			options: [
-				{
-					type: 'textinput',
-					label: 'UserNumber',
-					id: 'un',
-					default: '1',
-					regex:   self.REGEX_NUMBER
-				},
-				{
-					type: 'dropdown',
-					label: 'Action',
-					id: 'cuelistaction',
-					default: '0',
-					choices: self.CHOICES_CUELIST
-				}
-			]
-		},
-		'cuelistGoto': {
-			label: 'Cuelist Go to cue',
-			options: [
-				{
-					type: 'textinput',
-					label: 'UserNumber',
-					id: 'un',
-					default: '1',
-					regex:   self.REGEX_NUMBER
-				},
-				{
-					type: 'textinput',
-					label: 'CueNumber',
-					id: 'cn',
-					default: '1',
-					regex:   self.REGEX_NUMBER
-				}
-			]
-		}
-	});
-}
-instance.prototype.action = function(action) {
-	var self = this;
-	var cmd;
-	self.checkFeedbacks();
-	if (action.action == 'playbackAtPercentage') {
-		if (self.config.IP !== undefined) {
-			var percentage = action.options.percentage;
-			percentage = percentage/100;
-			var cmd = "http://" + self.config.IP + ":4430/titan/script/2/Playbacks/FirePlaybackAtLevel?handle_userNumber=" + action.options.un + "&level_level=" + percentage + "&alwaysRefire=true";
-			self.system.emit('rest_get', cmd, function (err, result) {
-				if (err !== null) {
-					self.status(self.STATUS_ERROR, result.error.code);
-				}
-				else {
-					self.status(self.STATUS_OK);
-				}
-			});
-		}
-	}
-	else if (action.action == 'cuelistGo') {
-		if (self.config.IP !== undefined) {
-			var cuelistaction = "Play";
-			if (action.options.cuelistaction == '1')
+	/**
+	 * Creates the configuration fields for web config.
+	 *
+	 * @returns {Array} the config fields
+	 * @access public
+	 * @since 1.0.0
+	 */
+	config_fields() {
+		return [
 			{
-				cuelistaction = "GoBack";
-			}
-			var cmd = "http://" + self.config.IP + ":4430/titan/script/2/CueLists/" + cuelistaction + "?handle_userNumber=" + action.options.un;
-			self.system.emit('rest_get', cmd, function (err, result) {
-				if (err !== null) {
-					self.status(self.STATUS_ERROR, result.error.code);
-				}
-				else {
-					self.status(self.STATUS_OK);
-				}
-			});
-		}
-	}
-	else if (action.action == 'playbackFlash') {
-		if (self.config.IP !== undefined) {
-			var percentage = "1";
-			if (action.options.playbackaction == '1')
-			{
-				percentage = "0";
-			}
-			var cmd = "http://" + self.config.IP + ":4430/titan/script/2/Playbacks/FirePlaybackAtLevel?handle_userNumber=" + action.options.un + "&level_level=" + percentage + "&alwaysRefire=true";
-			self.system.emit('rest_get', cmd, function (err, result) {
-				if (err !== null) {
-					self.status(self.STATUS_ERROR, result.error.code);
-				}
-				else {
-					self.status(self.STATUS_OK);
-				}
-			});
-		}
-		
-	}
-	else if (action.action == 'playbackSwop') {
-		if (self.config.IP !== undefined) {
-			var playbackaction = "SwopPlayback";
-			if (action.options.playbackaction == '1')
-			{
-				playbackaction = "ClearSwopPlayback";
-			}
-			var cmd = "http://" + self.config.IP + ":4430/titan/script/2/Playbacks/" + playbackaction + "?handle_userNumber=" + action.options.un;
-			self.system.emit('rest_get', cmd, function (err, result) {
-				if (err !== null) {
-					self.status(self.STATUS_ERROR, result.error.code);
-				}
-				else {
-					self.status(self.STATUS_OK);
-				}
-			});
-		}
-	}
-	else if (action.action == 'cuelistGoto') {
-		if (self.config.IP !== undefined) {
-			var cmd = "http://" + self.config.IP + ":4430/titan/script/2/CueLists/SetNextCue?handle_userNumber=" + action.options.un + "&stepNumber=" + action.options.cn;
-			self.system.emit('rest_get', cmd, function (err, result) {
-				if (err !== null) {
-					self.status(self.STATUS_ERROR, result.error.code);
-				}
-				else {
-					var cmd = "http://" + self.config.IP + ":4430/titan/script/2/CueLists/Play?handle_userNumber=" + action.options.un;
-					self.system.emit('rest_get', cmd, function (err, result) {
-						if (err !== null) {
-							self.status(self.STATUS_ERROR, result.error.code);
-						}
-						else {
-							self.status(self.STATUS_OK);
-						}
-					});
-				}
-			});
-		}
-	}
-}
-instance.prototype.init_feedbacks = function() {
-	var self = this;
-	var feedbacks = {};
-	feedbacks['image'] = {
-		label: 'Get picture legend of playback',
-		description: 'Updates the keys image to the image on your console',
-		options: [
-			{
-					type: 'textinput',
-					label: 'UserNumber',
-					id: 'un',
-					default: '1',
-					regex:   self.REGEX_NUMBER
+				type: 'text',
+				id: 'info',
+				width: 12,
+				label: 'Information',
+				value: 'This module comunicates to avolites titan desks thru the webapi protocol, the desk needs to be running version 10.0 or higher for this to work, some features might only work with higher software versions so make sure your avolites desk is always up to date'
 			},
+			{
+				type: 'textinput',
+				id: 'IP',
+				label: 'Desk IP',
+				regex: this.REGEX_IP,
+				width: 12
+			}
 		]
-	};
-	self.setFeedbackDefinitions(feedbacks);
-};
-instance.prototype.feedback = function(feedback, bank) {
-	var un = feedback["options"]["un"]
-	if(imagelist[un] != null) {
-		uploadimg = imagelist[un]
 	}
-	else {
-		uploadimg = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
+
+	/**
+	 * Clean up the instance before it is destroyed.
+	 *
+	 * @access public
+	 * @since 1.0.0
+	 */
+	destroy() {
+		if (this.pollInterval !== undefined) {
+			clearInterval(this.pollInterval);
+		}
+
+		this.debug("destroy", this.id);
 	}
-	var self = this;
-	return {
-		png64: uploadimg
-	};
-};
-setInterval(function() {
-	request.get('http://' + ipadr + ':4430/titan/handles', function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			var jsonobj = JSON.parse(Buffer.from(body).toString())
-			for (i = 0; i < jsonobj.length; i++) {
-				if(jsonobj[i]["icon"] != null) {
-					saveimage(jsonobj[i]["userNumber"]["hashCode"], jsonobj[i]["icon"])
+
+	/**
+	 * INTERNAL: Get an image from the console for a user number
+	 *
+	 * @param {number} un - the user number
+	 * @param {string} icon - URL of the icon to get
+	 * @since 1.1.0
+	 */
+	getImage(un, icon) {
+		this.request.get(icon, (error, response, body) => {
+			if (!error && response.statusCode == 200) {
+				this.imageList[un] = Buffer.from(body).toString('base64');
+			}
+		});
+	}
+
+	/**
+	 * Main initialization function called once the module
+	 * is OK to start doing things.
+	 *
+	 * @access public
+	 * @since 1.0.0
+	 */
+	init() {
+		this.sendCommand("get/System/SoftwareVersion");
+	}
+
+	/**
+	 * INTERNAL: initialize feedbacks.
+	 *
+	 * @access protected
+	 * @since 1.1.0
+	 */
+	initFeedbacks() {
+		var feedbacks = {};
+
+		feedbacks['image'] = {
+			label: 'Get picture legend of playback',
+			description: 'Updates the keys image to the image on your console',
+			options: [
+				this.FIELD_USERNUMBER
+			],
+			callback: (feedback, bank) => {
+				if (this.imageList[feedback.options.un] !== undefined) {
+					return {
+						png64: this.imageList[feedback.options.un]
+					}
+				}
+				else {
+					return {
+						png64: "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
+					}
 				}
 			}
+		};
+
+		this.setFeedbackDefinitions(feedbacks);
+	}
+
+	/**
+	 * INTERNAL: Request the user number images
+	 *
+	 * @since 1.1.0
+	 */
+	pollImages() {
+		this.request.get('http://' + this.config.IP + ':4430/titan/handles', (error, response, body) => {
+			if (!error && response.statusCode == 200) {
+				var jsonobj = JSON.parse(Buffer.from(body).toString());
+				for (var i = 0; i < jsonobj.length; i++) {
+					if(jsonobj[i]["icon"] != null) {
+						this.getImage(jsonobj[i]["userNumber"]["hashCode"], jsonobj[i]["icon"])
+					}
+				}
+			}
+
+			this.checkFeedbacks('image');
+		});
+	}
+
+	/**
+	 * INTERNAL: Send a command
+	 *
+	 * @param {string} uri - URI of the command to send
+	 * @returns {boolean} the command result
+	 * @since 1.1.0
+	 */
+	sendCommand(uri) {
+		if (this.config.IP !== undefined && uri !== undefined) {
+			var cmd = "http://" + this.config.IP + ":4430/titan/" + uri;
+
+			this.system.emit('rest_get', cmd, function (err, result) {
+				if (err !== null) {
+					this.status(this.STATUS_ERROR, result.error.code);
+					return false;
+				}
+				else {
+					this.status(this.STATUS_OK);
+
+					if (this.pollInterval === undefined) {
+						this.pollInterval = setInterval(this.pollImages.bind(this), 5000);
+					}
+
+					return true;
+				}
+			});
 		}
-	});
-}, 5000);
-function saveimage(un, icon) {
-	request.get(icon, function (error2, response2, body2) {
-		if (!error2 && response2.statusCode == 200) {
-			imagelist[un] = Buffer.from(body2).toString('base64')
+	}
+
+	/**
+	 * Process an updated configuration array.
+	 *
+	 * @param {Object} config - the new configuration
+	 * @access public
+	 * @since 1.0.0
+	 */
+	updateConfig(config) {
+		if (this.pollInterval !== undefined) {
+			clearInterval(this.pollInterval);
 		}
-	});
+
+		this.config = config;
+		this.actions();
+
+		this.sendCommand("get/System/SoftwareVersion");
+	}
 }
-instance_skel.extendedBy(instance);
+
 exports = module.exports = instance;
