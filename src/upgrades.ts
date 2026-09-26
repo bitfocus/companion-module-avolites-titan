35cd2a00dff786354167bcfbfa50d337712463f4
Vincent BOUQUET <contact@vincent-bouquet.fr>
2026-09-26 13:52:24 +0200

    Enhance Cuelist functionality to support Companion variables for cue numbers

--- src/upgrades.ts
+++ src/upgrades.ts
@@ -22,7 +22,7 @@ const upgradeV2_0_0: CompanionStaticUpgradeScript<ModuleConfig> = (
 }
 
 export const getUpgradeScripts = (): CompanionStaticUpgradeScript<ModuleConfig>[] => {
-	return [upgradeV2_0_0, upgradeCueListPresets, upgradeConfigPort]
+	return [upgradeV2_0_0, upgradeCueListPresets, upgradeConfigPort, upgradeCueNumberVariables]
 }
 
 // Repair saved buttons created by the old GO/BACK presets without changing IDs.
@@ -42,4 +42,12 @@ const upgradeConfigPort: CompanionStaticUpgradeScript<ModuleConfig> = (_context,
 		config.port ??= 4430
 	}
 	return { updatedConfig: config, updatedActions: [], updatedFeedbacks: [] }
+}
+
+const upgradeCueNumberVariables: CompanionStaticUpgradeScript<ModuleConfig> = (_context, props) => {
+	const updatedActions = props.actions.filter(
+		(action) => action.actionId === 'cuelistSetNextCue' && typeof action.options.cn === 'number',
+	)
+	for (const action of updatedActions) action.options.cn = String(action.options.cn)
+	return { updatedConfig: null, updatedActions, updatedFeedbacks: [] }
 }
